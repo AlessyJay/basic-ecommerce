9fa1e2279844fcaec95ce3829504ca824afe0638
AlessyJay <alessyraffaello@gmail.com>
2026-02-15 00:31:25 +0700

    Add more ports to the cors.

--- basic-ecommerce/Program.cs
+++ basic-ecommerce/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "https://localhost:3000")
+        policy.WithOrigins("http://localhost:3000", "https://localhost:3000", "https://localhost:5173", "http://localhost:5173")
         .AllowAnyHeader()
         .AllowAnyMethod()
         .AllowCredentials();
